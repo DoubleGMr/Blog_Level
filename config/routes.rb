d85884fac3d436d379c602c3630e1007b208e149
Jasonly <yao1508728331@gmail.com>
2018-03-22 21:29:44 +0800

    Make a basic User model(include secure password)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'pages#index'
   get '/about',to:'pages#about'
   get '/contact',to:'pages#contact'
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get pages_index_url
+    get root_url
     assert_response :success
   end
 
   test "should get about" do
-    get pages_about_url
+    get about_url
     assert_response :success
   end
 
   test "should get contact" do
-    get pages_contact_url
+    get contact_url
     assert_response :success
   end
 
