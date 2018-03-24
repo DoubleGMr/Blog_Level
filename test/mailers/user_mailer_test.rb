bbe4fb8859d707a98cdbe484086dc30c4cc55e52
Jasonly <yao1508728331@gmail.com>
2018-03-24 21:39:20 +0800

    change the email from

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -8,7 +8,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["noreply@example.com"], mail.from
+    assert_equal ["wanting@blog.com"], mail.from
     body_content = mail.body.encoded.split(/\r\n/).map{|i| Base64.decode64(i)}.join
     assert_match user.name, body_content
     assert_match user.activation_token, body_content
